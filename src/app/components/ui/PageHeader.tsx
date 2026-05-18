04d2059cee36dedf909459564127d16162e381f6
jithendrabathala <jithendrabathala@gmail.com>
2026-05-18 20:44:58 +0530

    fix: build issues

--- src/app/components/ui/PageHeader.tsx
+++ src/app/components/ui/PageHeader.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-export const PageHeader = ({ theme = "dark" }: { theme: string }) => {
+export const PageHeader = ({ theme = "dark" }: { theme?: string }) => {
   return (
     <motion.header
       initial={{ opacity: 0, y: -12 }}
