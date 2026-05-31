b14a3bf23a2cfbb78f265fb12196944b572c3380
jithendrabathala <jithendrabathala@gmail.com>
2026-06-01 02:58:34 +0530

    add: fav icon, title and meta data

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,11 +1,16 @@
-"use client";
-
+import type { Metadata } from "next";
 import ApplicationSection from "../components/application/ApplicationSection";
 import FAQs from "../components/application/FAQs";
 import StatsBoard from "../components/application/StatsBoard";
 import SupplyChain from "../components/application/SupplyChain";
 import WhatNext from "../components/application/WhatNext";
 import { PageHeader } from "../components/ui/PageHeader";
+
+export const metadata: Metadata = {
+  title: "Book Your Account Setup Call | PeptiPharmaRX",
+  description:
+    "Your application is in. Book your 30-minute account setup call to confirm your practice details. We will walk you through our catalog of 80+ peptides and get your account live.",
+};
 
 export default function ApplicationPage() {
   return (
